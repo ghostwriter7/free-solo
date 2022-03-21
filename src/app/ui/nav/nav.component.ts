2797fd3e2e975d862d7b17aac0a91ee12b33fe24
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-03-21 07:02:06 +0100

    Fixed bug with overflow on mobile menu

--- src/app/ui/nav/nav.component.ts
+++ src/app/ui/nav/nav.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { IconsService } from '../../core/services/icons.service';
-import { ThemeService } from '../../core/services';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { ThemeService, IconsService } from '../../core/services';
+import { BehaviorSubject, Subject, take, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -9,25 +9,41 @@ import { ThemeService } from '../../core/services';
 })
 export class NavComponent implements OnInit {
   public menuItems: { title: string; path: string; }[] = [
-    { title: 'Home', path: '/home' },
-    { title: 'Projects', path: '/projects' }
+    {title: 'Home', path: '/home'},
+    {title: 'Projects', path: '/projects'}
   ];
   public isMenuActive = false;
+  public toggleMenu$ = new Subject<boolean>();
+
+  @HostListener('window:resize', ['$event']) onWindowResize(e: Event) {
+    if ((e.target as Window).innerWidth > 600) {
+      this._renderer2.removeStyle(document.body, 'overflow-y');
+    }
+  }
 
   constructor(
     public iconsService: IconsService,
-    private _themeService: ThemeService
-    ) { }
+    private _themeService: ThemeService,
+    private _renderer2: Renderer2
+  ) {}
 
   ngOnInit(): void {
+    this.toggleMenu$.subscribe({
+      next: (state) => {
+        if (window.innerWidth < 600) {
+          state ? this._renderer2.setStyle(document.body, 'overflow-y', 'hidden') :
+            this._renderer2.removeStyle(document.body, 'overflow-y');
+        }
+      }
+    });
   }
 
   public onToggleMenu(): void {
     this.isMenuActive = !this.isMenuActive;
+    this.toggleMenu$.next(this.isMenuActive);
   }
 
   public onToggleTheme(): void {
     this._themeService.toggleTheme();
   }
-
 }
