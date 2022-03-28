2c53e3c1b1052d615aee31c411a5e810e30848b8
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-03-28 20:30:25 +0200

    Routing fixed, small fixes in mobie menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule ) }
+  { path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule ) },
+  { path: '**', redirectTo: '/home'}
 ];
 
 @NgModule({
