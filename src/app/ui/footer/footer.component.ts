0aa837dd97d594f8f2b0eb2d19bf56c9040015af
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-03-28 16:59:16 +0200

    Styles updated

--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IconsService } from '../../core/services';
 
 @Component({
@@ -6,17 +6,12 @@ import { IconsService } from '../../core/services';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
   public links: { icon: string; path: string; tooltip: string }[] = [
     { icon: 'github', path: 'https://github.com/ghostwriter7', tooltip: 'My GitHub profile' },
     { icon: 'linkedin', path: 'https://www.linkedin.com/in/hubert-rozplochowski-266a4a220/', tooltip: 'My LinkedIn profile' },
     { icon: 'movie', path: 'https://www.youtube.com/watch?v=-sXGkJsTrY8&t=7s', tooltip: 'Juggling!' }
   ];
 
-
   constructor(public iconsService: IconsService) { }
-
-  ngOnInit(): void {
-  }
-
 }
