625b594177a358ce4ea5b2a7b6b2f59c34bdd5cc
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-03-29 19:57:44 +0200

    Puzzle animations simplified to handle Firefox properly

--- src/app/pages/home/quiz-item/quiz-item.component.ts
+++ src/app/pages/home/quiz-item/quiz-item.component.ts
@@ -1,29 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IconsService } from '../../../core/services';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-quiz-item',
   templateUrl: './quiz-item.component.html',
   styleUrls: ['./quiz-item.component.scss'],
-  animations: [
-    trigger('flip', [
-      state('show', style({transform: 'rotateY(180deg)'})),
-      state('hide', style({ transform: 'rotateY(0deg)'})),
-      transition('show <=> hide', animate('0.3s ease-in-out'))
-    ])
-  ]
 })
-export class QuizItemComponent implements OnInit {
+export class QuizItemComponent {
   @Input() icon!: string;
   @Input() isSuccess = false;
   @Input() isWrong = false;
   public isHidden = true;
 
   constructor(public iconsService: IconsService) { }
-
-  ngOnInit(): void {
-  }
 
   public toggle(): void {
     this.isHidden = !this.isHidden;
