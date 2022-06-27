d44e2ee09824623ed460c8f7a9a3353627e40e89
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-06-27 19:23:49 +0200

    Firebase installed

---
+++ src/app/core/libs/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5eNVkRoTryV3TFJg_lBjPt82KB9phJNo",
+  authDomain: "portfolio-1bc80.firebaseapp.com",
+  projectId: "portfolio-1bc80",
+  storageBucket: "portfolio-1bc80.appspot.com",
+  messagingSenderId: "432221206968",
+  appId: "1:432221206968:web:69a70414ba620766fc774d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
